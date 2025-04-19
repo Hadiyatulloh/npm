25dc33fd809b88c010a82526650cfb9289c2af79
Hadiyatulloh <hadiyatullohfayzullayev@gmail.com>
2025-04-19 16:46:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+var typed = new Typed('#firstExercise_h1', {
+    strings: ['Text Gradients', ''],
+    typeSpeed: 80,
+    backSpeed: 40,
+    backDelay: 3700,
+    loop: true,
+
+
+});
+var typed2 = new Typed('#firstExercise_p', {
+    strings: ['Make your headlines stand out', ''],
+    typeSpeed: 80,
+    backSpeed: 40,
+    startDelay: 1700,
+    backDelay: 1500,
+    loop: true,
+
+
+});
+var typed3 = new Typed('#secondh1', {
+    strings: ['Design is intelligence <br> made visible', ''],
+    typeSpeed: 70,
+
+    backSpeed: 300,
+    backDelay: 1500,
+    loop: true,
+
+});
+var typed4 = new Typed('#secondp', {
+    strings: ['Interaction design isn’t merely a matter of aesthetic choice; rather, <br> it is based on an understanding of users and cognitive principles.', ''], // Массив строк, которые будут печататься
+    typeSpeed: 20,
+    backSpeed: 70,
+    startDelay: 1700,
+    loop: true,
+
+});
+
+
+var typed3 = new Typed('#second2h1', {
+    strings: ['IMG qoyomadim'],
+    typeSpeed: 70,
+
+    backSpeed: 300,
+    backDelay: 1500,
+    loop: true,
+
+});
